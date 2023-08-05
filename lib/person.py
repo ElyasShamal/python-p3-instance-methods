c8978d229f33e2ff4390893c85165859bd6f1a16
Elyas <Elyas.shamal2@gmail.com>
2023-08-05 15:09:52 -0400

    completed

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,17 @@
 class Person:
     # Class body goes here
 
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print('The person is walking.')    
+
+
     #Instance method definition
-    pass
+
+elyas = Person()
+elyas.talk()
+elyas.walk()
+    
+
